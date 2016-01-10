500bd0c66b8d0720beebf806609347aae71b479e
Paul Joey Clark <joey@neuralyte.org>
2016-01-10 20:40:34 +0800

    Use className instead of class in JSX to avoid object property/keyword issue

--- components/Contacts.js
+++ components/Contacts.js
@@ -8,21 +8,21 @@ class Contact extends Component {
     let contact = this.props.contact
     return (
       <div>
-        <div class="thumbnail">
-          <img class="media-object" src="app/img/faces/{ contact.avatar }" />
+        <div className="thumbnail">
+          <img className="media-object" src="app/img/faces/{ contact.avatar }" />
         </div>
-        <div class="media-heading">
+        <div className="media-heading">
           <h3>
             { contact.name }
             <small>
-              <a href="#contacts/edit/{ contact.id }"><span class="glyphicon glyphicon-pencil"></span></a>
-              <a href="#contacts/delete/{ contact.id }" class="delete-contact">
-                <span class="glyphicon glyphicon-trash"></span>
+              <a href="#contacts/edit/{ contact.id }"><span className="glyphicon glyphicon-pencil"></span></a>
+              <a href="#contacts/delete/{ contact.id }" className="delete-contact">
+                <span className="glyphicon glyphicon-trash"></span>
               </a>
             </small>
           </h3>
         </div>
-        <div class="media-body">
+        <div className="media-body">
           <dl>
             <dt>Phone Number:</dt>
             <dd>{ contact.tel }</dd>
@@ -39,18 +39,21 @@ class Contact extends Component {
 class Contacts extends Component {
   render() {
     const { state } = this.props
-    console.log("[Contacts.js] state:", state);
+    //console.log("[Contacts.js] state:", state);
     // mapDispatchToProps has also put all of our dispatch functions into this.props?
     return (
       <div>
-        <h2 class="page-header text-center">List of contacts</h2>
-        <p class="text-center">
-          <a href="#contacts/new" class="btn btn-lg btn-outline">Add Contact</a>
+        <h2 className="page-header text-center">List of contacts</h2>
+        <p className="text-center">
+          <a href="#contacts/new" className="btn btn-lg btn-outline">Add Contact</a>
         </p>
-        <ul class="media-list row contacts-container">
-          {state.contacts.contacts.map( contact =>
-            <Contact contact={contact} />
-          )}
+        <ul className="media-list row contacts-container">
+          {
+            // TODO: Nobody knows why we have contactception here
+            state.contacts.contacts.map( contact =>
+              <Contact contact={contact} />
+            )
+          }
         </ul>
       </div>
     )
@@ -78,32 +81,32 @@ class Contacts extends Component {
     </script>
 
     <script type="text/template" id="tpl-new-contact">
-      <h2 class="page-header text-center"><%= isNew ? 'Create' : 'Edit' %> Contact</h2>
-      <form role="form" class="form-horizontal contract-form">
-        <div class="form-group">
-          <label class="col-sm-4 control-label">Full name:</label>
-          <div class="col-sm-6">
-            <input type="text" class="form-control contact-name-input" value="<%- name %>">
+      <h2 className="page-header text-center"><%= isNew ? 'Create' : 'Edit' %> Contact</h2>
+      <form role="form" className="form-horizontal contract-form">
+        <div className="form-group">
+          <label className="col-sm-4 control-label">Full name:</label>
+          <div className="col-sm-6">
+            <input type="text" className="form-control contact-name-input" value="<%- name %>">
           </div>
         </div>
-        <div class="form-group">
-          <label class="col-sm-4 control-label">Email address:</label>
-          <div class="col-sm-6">
-            <input type="email" class="form-control contact-email-input" value="<%- email %>">
+        <div className="form-group">
+          <label className="col-sm-4 control-label">Email address:</label>
+          <div className="col-sm-6">
+            <input type="email" className="form-control contact-email-input" value="<%- email %>">
           </div>
         </div>
-        <div class="form-group">
-          <label class="col-sm-4 control-label">Telephone number:</label>
-          <div class="col-sm-6">
-            <input type="tel" class="form-control contact-tel-input" value="<%- tel %>">
+        <div className="form-group">
+          <label className="col-sm-4 control-label">Telephone number:</label>
+          <div className="col-sm-6">
+            <input type="tel" className="form-control contact-tel-input" value="<%- tel %>">
           </div>
         </div>
-        <div class="form-group">
-          <div class="col-sm-offset-4 col-sm-3">
-            <button type="submit" class="btn btn-outline btn-lg btn-block">Submit</button>
+        <div className="form-group">
+          <div className="col-sm-offset-4 col-sm-3">
+            <button type="submit" className="btn btn-outline btn-lg btn-block">Submit</button>
           </div>
-          <div class="col-sm-3">
-            <a href="#contacts" class="btn btn-outline btn-lg btn-block">Cancel</a>
+          <div className="col-sm-3">
+            <a href="#contacts" className="btn btn-outline btn-lg btn-block">Cancel</a>
           </div>
         </div>
       </form>
