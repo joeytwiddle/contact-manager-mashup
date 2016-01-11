46df27d336277a365e63dd5eba4d6fa4dad1e1a9
Paul Joey Clark <joey@neuralyte.org>
2016-01-11 10:41:47 +0800

    Added and integrated redux-simple-router

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './containers/App'
+// TODO: Extract routes into a routes.js/jsx file
+import Contacts from './components/Contacts'
+import ContactForm from './components/ContactForm'
 import configureStore from './store/configureStore'
+import { Router, Route } from 'react-router'
+import { createHistory } from 'history'
+import { syncReduxAndRouter } from 'redux-simple-router'
 
 const store = configureStore()
 
+const history = createHistory()
+syncReduxAndRouter(history, store)
+
 render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <Route path="#contacts" component={Contacts}/>
+        <Route path="#contacts/new" component={ContactForm}/>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
--- reducers/index.js
+++ reducers/index.js
@@ -1,8 +1,10 @@
+import { routeReducer } from 'redux-simple-router'
 import { combineReducers } from 'redux'
 import contacts from './contacts'
 
 const rootReducer = combineReducers({
-  contacts
+  contacts,
+  routing: routeReducer
 })
 
 export default rootReducer
