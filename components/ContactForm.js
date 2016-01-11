4b81b6923823ac4cf49f047e36e7ede6b1f7269d
Paul Joey Clark <joey@neuralyte.org>
2016-01-11 10:42:55 +0800

    WIP Added initial ContactForm component

---
+++ components/ContactForm.js
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react'
+
+class ContactForm extends Component {
+  render() {
+    let isNew = this.props.isNew
+    let contact = this.props.contact
+    return (
+      <div>
+        <h2 className="page-header text-center">{ isNew ? 'Create' : 'Edit' } Contact</h2>
+        <form role="form" className="form-horizontal contract-form">
+          <div className="form-group">
+            <label className="col-sm-4 control-label">Full name:</label>
+            <div className="col-sm-6">
+              <input type="text" className="form-control contact-name-input" value="{ contact.name }">
+            </div>
+          </div>
+          <div className="form-group">
+            <label className="col-sm-4 control-label">Email address:</label>
+            <div className="col-sm-6">
+              <input type="email" className="form-control contact-email-input" value="{ contact.email }">
+            </div>
+          </div>
+          <div className="form-group">
+            <label className="col-sm-4 control-label">Telephone number:</label>
+            <div className="col-sm-6">
+              <input type="tel" className="form-control contact-tel-input" value="{ contact.tel }">
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-offset-4 col-sm-3">
+              <button type="submit" className="btn btn-outline btn-lg btn-block">Submit</button>
+            </div>
+            <div className="col-sm-3">
+              <a href="#contacts" className="btn btn-outline btn-lg btn-block">Cancel</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default ContactForm
