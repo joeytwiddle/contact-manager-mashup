4a16ee7b76f72acbb479b76c8f5d4a2af51e0163
Paul Joey Clark <joey@neuralyte.org>
2016-01-11 23:04:12 +0800

    Creating a new contact now works

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -2,30 +2,59 @@ import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
 class ContactForm extends Component {
+
+  onChangeHandlerFor(field) {
+    return (event) => {
+      let value = event.target.value
+      let update = {}
+      update[field] = value
+      this.setState(update);
+    }
+  }
+
   render() {
-    console.log("[ContactForm.js] this.props:", this.props);
-    let isNew = this.props.isNew
-    let contact = this.props.contact || {}
+    //console.log("[ContactForm.js] this.props:", this.props);
+
+    let { isNew, dispatch, contact = {} } = this.props
+
+    // It is better to wait for form 'submit' event than for a click on the submit button
+    // Because forms could also be submitted by user hitting <Enter> in one of the form inputs, or <Space> on the submit button.
+    const onSubmit = (event) => {
+      event.preventDefault()
+      console.log("[ContactForm.js] this:", this);
+      let newContactDetails = this.state
+      console.log("[ContactForm.js] newContactDetails:", newContactDetails);
+      console.log("[ContactForm.js] this.props.dispatch:", this.props.dispatch);
+
+      //dispatch( this.props.addContact(newContactDetails) )
+      // It seems we don't need to dispatch, this is enough
+      this.props.addContact(newContactDetails)
+
+      // This feels naughty.  It might not be the proper way.
+      // (It likely wouldn't work at all if we hadn't added the "optional" history in index.js)
+      this.props.history.pushState("/contacts")
+    }
+
     return (
       <div>
         <h2 className="page-header text-center">{ isNew ? 'Create' : 'Edit' } Contact</h2>
-        <form role="form" className="form-horizontal contract-form">
+        <form role="form" className="form-horizontal contract-form" onSubmit={onSubmit}>
           <div className="form-group">
             <label className="col-sm-4 control-label">Full name:</label>
             <div className="col-sm-6">
-              <input type="text" className="form-control contact-name-input" value={ contact.name } />
+              <input type="text" className="form-control contact-name-input" value={ contact.name } onChange={this.onChangeHandlerFor('name')}/>
             </div>
           </div>
           <div className="form-group">
             <label className="col-sm-4 control-label">Email address:</label>
             <div className="col-sm-6">
-              <input type="email" className="form-control contact-email-input" value={ contact.email } />
+              <input type="email" className="form-control contact-email-input" value={ contact.email } onChange={this.onChangeHandlerFor('email')}/>
             </div>
           </div>
           <div className="form-group">
             <label className="col-sm-4 control-label">Telephone number:</label>
             <div className="col-sm-6">
-              <input type="tel" className="form-control contact-tel-input" value={ contact.tel } />
+              <input type="tel" className="form-control contact-tel-input" value={ contact.tel } onChange={this.onChangeHandlerFor('tel')}/>
             </div>
           </div>
           <div className="form-group">
@@ -44,6 +73,7 @@ class ContactForm extends Component {
 
 // TODO
 ContactForm.propTypes = {
+  //addContact: PropTypes.func.isRequired
 }
 
 export default ContactForm
--- containers/NewContactForm.js
+++ containers/NewContactForm.js
@@ -1,5 +1,12 @@
 import { connect } from 'react-redux'
 import ContactForm from '../components/ContactForm';
+import { addContact } from '../actions/contacts.js'
+
+import * as ContactManagerActions from '../actions/contacts'
+
+console.log("[NewContactForm.js] ContactManagerActions:", ContactManagerActions);
+
+import { bindActionCreators } from 'redux'
 
 function mapStateToProps(state) {
   return {
@@ -8,4 +15,18 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(ContactForm);
+// This provides 'dispatch' to the component, but no actions
+//export default connect(mapStateToProps)(ContactForm)
+
+// This provides the 'addContact' action to the component, but loses the 'dispatch' function!
+// Oh that appears to be ok.  We don't need to dispatch function (unless we want to 
+export default connect(mapStateToProps, {
+  addContact
+})(ContactForm)
+
+// This passed all the action constructor functions, but no 'dispatch' function.
+// Maybe that's ok; they can bind to 'dispatch' if they want to (as seen in the counter example)
+//function mapDispatchToProps(dispatch) {
+  //return bindActionCreators(ContactManagerActions, dispatch)
+//}
+//export default connect(mapStateToProps, mapDispatchToProps)(ContactForm)
