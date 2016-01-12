399693eeddd07cb4a7d9a834765bd445c8843f00
Paul Joey Clark <joey@neuralyte.org>
2016-01-12 10:29:30 +0800

    Cleanup: removed a log, re-ordered imports

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 
+import configureStore from './store/configureStore'
+import { Router, Route, Redirect, Link } from 'react-router'
+import { createHistory } from 'history'
+import { syncReduxAndRouter } from 'redux-simple-router'
+
 //TODO: Extract routes into a routes.js/jsx file
 //import App from './containers/App'
 import Contacts from './containers/Contacts'
 import NewContactForm from './containers/NewContactForm'
 import EditContactForm from './containers/EditContactForm'
-
-import configureStore from './store/configureStore'
-import { Router, Route, Redirect, Link } from 'react-router'
-console.log("[index.js] Link:", Link);
-import { createHistory } from 'history'
-import { syncReduxAndRouter } from 'redux-simple-router'
 
 const store = configureStore()
 console.log("[index.js] store.getState():", store.getState());
